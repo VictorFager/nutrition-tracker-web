a40994654428b1fc484edfb0673c0fd0851eebaa
VictorFager <victorfager@gmail.com>
2025-06-15 12:05:45 +0200

    WIP: responsive tables

--- app/ingredients/page.tsx
+++ app/ingredients/page.tsx
@@ -1,9 +1,22 @@
 import IngredientTable from "@/components/item-table";
+import cStyles from "@/components/components.module.css";
 
 export default function IngredientsPage() {
   return (
     <>
-      <IngredientTable />
+      <div className={`${cStyles.container} ${cStyles.flexNoShrink}`}>
+        <p className={cStyles.test}>
+          En test container för att se hur saker funkar
+        </p>
+        <p className={cStyles.test}>En och annan p tagg för att fylla ut</p>
+        <p className={cStyles.test}>En och annan p tagg för att fylla ut</p>
+        <p className={cStyles.test}>En och annan p tagg för att fylla ut</p>
+      </div>
+      <div
+        className={`${cStyles.container} ${cStyles.flexNoShrink} ${cStyles.fullHeight}`}
+      >
+        <IngredientTable />
+      </div>
     </>
   );
 }
--- components/item-table.tsx
+++ components/item-table.tsx
@@ -4,15 +4,18 @@ import cStyles from "./components.module.css";
 export default async function IngredientTable() {
   const ingredients = await fetchIngredients();
   return (
-    <div className={cStyles.container}>
-      <table className={cStyles.itemTable}>
+    <div className={cStyles.tableContainer}>
+      <table className={cStyles.table}>
         <thead>
           <tr>
             <th>Beskrivning</th>
             <th>Energi</th>
             <th>Fett</th>
-            <th>Kolh.</th>
-            <th>Prot.</th>
+            <th>Kolhyd.</th>
+            <th>Protein</th>
+            <th>Socker</th>
+            <th>Fiber</th>
+            <th>Salt</th>
           </tr>
         </thead>
         <tbody>
@@ -23,6 +26,9 @@ export default async function IngredientTable() {
               <td>{ingredient.fat / 1000} g</td>
               <td>{ingredient.carbohydrate / 1000} g</td>
               <td>{ingredient.protein / 1000} g</td>
+              <td>{ingredient.sugar / 1000} g</td>
+              <td>{ingredient.fiber / 1000} g</td>
+              <td>{ingredient.salt / 1000} g</td>
             </tr>
           ))}
         </tbody>
